db2e6a3f85c71bdb26f8f9713804e431cd4d7afe
APaud3l <aamod.paudel@coderacademy.edu.au>
2024-11-30 11:26:09 +1100

    feat: Implemented ContextProvider on existing project

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,22 @@ import Home from './components/Home';
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
 import EditTask from './components/EditTask';
+import TaskProvider from './contexts/TaskContext';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Home />
-        <Routes>
-          <Route path='/tasks' element={<Tasks />} />
-          <Route path='/add-task' element={<AddTask />} />
-          <Route path='/edit-task/:taskId' element={<EditTask />} />
-        </Routes>
-      </div>
-    </Router>
+    <TaskProvider>
+      <Router>
+        <div>
+          <Home />
+          <Routes>
+            <Route path='/tasks' element={<Tasks />} />
+            <Route path='/add-task' element={<AddTask />} />
+            <Route path='/edit-task/:taskId' element={<EditTask />} />
+          </Routes>
+        </div>
+      </Router>
+    </TaskProvider>
 
   )
 }
